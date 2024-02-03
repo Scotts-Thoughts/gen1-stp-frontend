ef45f570cae1fee5063e7e9b65ae30d2ddac36f2
Austin <quinmarino@gmail.com>
2024-02-02 22:56:05 -0500

    fixed window resizing and error on minimizing

--- main.js
+++ main.js
@@ -17,10 +17,10 @@ function createWindow() {
     },
   })
   const contentBounds = mainWindow.getContentBounds()
-    const titleBarHeight = 1080 - contentBounds.height
-    const borderWidth = (1920 - contentBounds.width) / 2
-    if(titleBarHeight > 0 && borderWidth > 0)
-    mainWindow.setSize(1920 + borderWidth * 2, 1080 + titleBarHeight)
+  const titleBarHeight = 1080 - contentBounds.height
+  const borderWidth = (1920 - contentBounds.width) / 2
+  if(titleBarHeight > 0 && borderWidth > 0)
+  mainWindow.setSize(1920 + borderWidth * 2, 1080 + titleBarHeight)
 
   mainWindow.on('resize', () => {
     // Get the new size of the window
@@ -36,15 +36,13 @@ function createWindow() {
     else {
       zoomFactor = horizontalZoomFactor;
     }
-    mainWindow.webContents.setZoomFactor(zoomFactor);
+    if(zoomFactor > 0) {
+      mainWindow.webContents.setZoomFactor(zoomFactor);
+    }
   })
 
   mainWindow.on('ready-to-show', () => {
-    // const contentBounds = mainWindow.getContentBounds()
-    // const titleBarHeight = 1080 - contentBounds.height
-    // const borderWidth = (1920 - contentBounds.width) / 2
-    // if(titleBarHeight > 0 && borderWidth > 0)
-    // mainWindow.setSize(1920 + borderWidth * 2, 1080 + titleBarHeight)
+    //zoom the window appropriately before showing
     const { width } = mainWindow.getContentBounds();
     const { height } = mainWindow.getContentBounds();
     const horizontalZoomFactor = width / 1920;
@@ -57,7 +55,9 @@ function createWindow() {
     else {
       zoomFactor = horizontalZoomFactor;
     }
-    mainWindow.webContents.setZoomFactor(zoomFactor);
+    if(zoomFactor > 0) {
+      mainWindow.webContents.setZoomFactor(zoomFactor);
+    }
     mainWindow.show()
     // mainWindow.webContents.openDevTools()
   })
