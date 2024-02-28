682a08439646c16d7bdc709062749da2cbc87596
Scotts-Thoughts <99685188+Scotts-Thoughts@users.noreply.github.com>
2024-02-27 21:49:11 -0700

    fade bug
    - stats background for enemy pokemon are not fading how I want it to

--- app/vue.js
+++ app/vue.js
@@ -1931,6 +1931,9 @@ const app = Vue.createApp({
               faint: isFainted
                 ? "filter: drop-shadow(2px 2px 2px #000) saturate(1.3) grayscale(100%); opacity: .5;"
                 : "filter: drop-shadow(2px 2px 2px #000) saturate(1.3) grayscale(0%);",
+              faint_stats_background: isFainted
+                ? "filter: grayscale(100%); opacity: .3;"
+                : "filter: grayscale(0%);",
               faintStats: isFainted
                 ? "filter: grayscale(100%); opacity: .4;"
                 : "filter: grayscale(0%);",
