db498d243bb6c515fd9785cabd43e4f60f67b588
StringEpsilon <StringEpsilon@gmail.com>
2025-07-16 01:28:53 +0200

    Fix movepool.js

--- app/components/Movepool.js
+++ app/components/Movepool.js
@@ -1,8 +1,8 @@
-const template = `
+const template = /*html*/`
     <div class="movepool-container">
         <table class="movepoolDemo" cellspacing="0" :style="mew_movepool_style">
             <tbody>
-                <tr class="movepool_header" :style="{'background-color': 'var(--'+ui_type_color_modifier+starting_type_fix[1]+')', 'outline': '2px solid #000'}">
+                <tr class="movepool_header" :style="{'background-color': \`var(--\${ui_type_color_modifier}\${starting_type_fix[1]})\`, 'outline': '2px solid #000'}">
                     <th class="movepool_header movepool_way">Way</th>
                     <th class="movepool_header movepool_move">Move</th>
                     <th class="movepool_header movepool_type">Type</th>
@@ -10,26 +10,26 @@ const template = `
                     <th class="movepool_header movepool_tableC">Acc.</th>
                     <th class="movepool_header movepool_tableC">PP</th>
                 </tr>
-                <tr v-for="(x, index) in getMovepool(pokemon_version_specific_data, g1MoveData, tmhmMapping, s1dynamicReset?.species?.value).initial" :class="{ 'first-row': index === 0 }">
+                <tr v-for="(x, index) in getMovepool(pokemon_version_specific_data, moveData, tmhmMapping, dynamicReset?.species?.value).initial" :class="{ 'first-row': index === 0 }">
                     <td class="movepool_way">1</td>
                     <td class="movepool_move">{{x.Move}}</td>
-                    <td class="movepool_type" :style="{'background-color': 'var(--'+ui_type_color_modifier+x.Type.toLowerCase()+')'}">{{x.Type}}</td>
+                    <td class="movepool_type" :style="{'background-color': \`var(--\${ui_type_color_modifier}\${x.Type.toLowerCase()})\`}">{{x.Type}}</td>
                     <td class="movepool_tableC">{{x.Power}}</td>
                     <td class="movepool_tableC">{{x.Accuracy}}%</td>
                     <td class="movepool_tableC">{{x.PP}}</td>
                 </tr>
-                <tr v-for="x in getMovepool(pokemon_version_specific_data, g1MoveData, tmhmMapping, s1dynamicReset?.species?.value).level">
+                <tr v-for="x in getMovepool(pokemon_version_specific_data, moveData, tmhmMapping, dynamicReset?.species?.value).level">
                     <td class="movepool_way">{{x.Level}}</td>
                     <td class="movepool_move">{{x.Move}}</td>
-                    <td class="movepool_type" :style="{'background-color': 'var(--'+ui_type_color_modifier+x.Type.toLowerCase()+')'}">{{x.Type}}</td>
+                    <td class="movepool_type" :style="{'background-color': \`var(--\${ui_type_color_modifier}\${x.Type.toLowerCase()})\`}">{{x.Type}}</td>
                     <td class="movepool_tableC">{{x.Power}}</td>
                     <td class="movepool_tableC">{{x.Accuracy}}%</td>
                     <td class="movepool_tableC">{{x.PP}}</td>
                 </tr>
-                <tr v-for="x in getMovepool(pokemon_version_specific_data, g1MoveData, tmhmMapping, s1dynamicReset?.species?.value).tmhm">
+                <tr v-for="x in getMovepool(pokemon_version_specific_data, moveData, tmhmMapping, dynamicReset?.species?.value).tmhm">
                     <td class="movepool_way">{{x.tmhm}}</td>
                     <td class="movepool_move">{{x.Move}}</td>
-                    <td class="movepool_type" :style="{'background-color': 'var(--'+ui_type_color_modifier+x.Type?.toLowerCase()+')'}">{{x.Type}}</td>
+                    <td class="movepool_type" :style="{'background-color': \`var(--\${ui_type_color_modifier}\${x.Type?.toLowerCase()})\`}">{{x.Type}}</td>
                     <td class="movepool_tableC">{{x.Power}}</td>
                     <td class="movepool_tableC">{{x.Accuracy}}%</td>
                     <td class="movepool_tableC">{{x.PP}}</td>
@@ -44,20 +44,16 @@ const template = `
 module.exports = {
     template,
     props: [
-        "starter",
+        "starterName",
         "pokemon_version_specific_data",
-        "move_data",
-        "tmhm_mapping",
+        "moveData",
         "dynamicReset",
+        "tmhmMapping",
         "ui_saturation",
         "starting_type_fix",
     ],
     data() {
         return {
-            starterName: this.starter,
-            g1MoveData: this.move_data,
-            tmhmMapping: this.tmhm_mapping,
-            s1dynamicReset: this.dynamicReset,
             ui_type_color_modifier: "current_",
         }
     },
