e5494cec5fba107a6a5b2d8274976dab5a140a9d
Scotts-Thoughts <99685188+Scotts-Thoughts@users.noreply.github.com>
2025-07-16 12:20:28 -0600

    keyhook refactor - not working
    - needs to link the key presses back to the frontend.js properties they need to modify

---
+++ app/components/keyhook.js
@@ -0,0 +1,321 @@
+const Keys = {
+    "ESCAPE":         0x1B,
+    "BACKSPACE":      0x08,
+    "TAB":            0x09,
+    
+    "LEFT":           0x25,
+    "UP":             0x26,
+    "RIGHT":          0x27,
+    "DOWN":           0x28,
+    
+    "CAPS_LOCK":      0x14,
+    "NUM_LOCK":       0x90,
+    "SCROLL_LOCK":    0x91,
+    
+    "SHIFT":          0x10,
+    "L_SHIFT":        0xA0,
+    "R_SHIFT":        0xA1,
+
+    "CTRL":           0x11,
+    "L_CTRL":         0xA2,
+    "R_CTRL":         0xA3,
+    
+    "ALT":            0x12,
+    "L_ALT":          0xA4,
+    "R_ALT":          0xA5,
+    
+    "L_WIN":          0x5B,
+    "R_WIN":          0x5C,
+
+    "0":              0x30,
+    "1":              0x31,
+    "2":              0x32,
+    "3":              0x33,
+    "4":              0x34,
+    "5":              0x35,
+    "6":              0x36,
+    "7":              0x37,
+    "8":              0x38,
+    "9":              0x39,
+
+    "A":              0x41,
+    "B":              0x42,
+    "C":              0x43,
+    "D":              0x44,
+    "E":              0x45,
+    "F":              0x46,
+    "G":              0x47,
+    "H":              0x48,
+    "I":              0x49,
+    "J":              0x4A,
+    "K":              0x4B,
+    "L":              0x4C,
+    "M":              0x4D,
+    "N":              0x4E,
+    "O":              0x4F,
+    "P":              0x50,
+    "Q":              0x51,
+    "R":              0x52,
+    "S":              0x53,
+    "T":              0x54,
+    "U":              0x55,
+    "V":              0x56,
+    "W":              0x57,
+    "X":              0x58,
+    "Y":              0x59,
+    "Z":              0x5A,
+
+    "NUMPAD0":        0x60,
+    "NUMPAD1":        0x61,
+    "NUMPAD2":        0x62,
+    "NUMPAD3":        0x63,
+    "NUMPAD4":        0x64,
+    "NUMPAD5":        0x65,
+    "NUMPAD6":        0x66,
+    "NUMPAD7":        0x67,
+    "NUMPAD8":        0x68,
+    "NUMPAD9":        0x69,
+    "SEPARATOR":      0x6C,
+    "DECIMAL":        0x6E,
+
+    "MULTIPLY":       0x6A,
+    "ADD":            0x6B,
+    "SUBTRACT":       0x6D,
+    "DIVIDE":         0x6F,
+    
+    "F1":             0x70,
+    "F2":             0x71,
+    "F3":             0x72,
+    "F4":             0x73,
+    "F5":             0x74,
+    "F6":             0x75,
+    "F7":             0x76,
+    "F8":             0x77,
+    "F9":             0x78,
+    "F10":            0x79,
+    "F11":            0x7A,
+    "F12":            0x7B,
+    "F13":            0x7C,
+    "F14":            0x7D,
+    "F15":            0x7E,
+    "F16":            0x7F,
+    "F17":            0x80,
+    "F18":            0x81,
+    "F19":            0x82,
+    "F20":            0x83,
+    "F21":            0x84,
+    "F22":            0x85,
+    "F23":            0x86,
+    "F24":            0x87,
+
+
+    "APPS":           0x5D,
+    "CLEAR":          0x0C,
+    "RETURN":         0x0D,
+    "PAUSE":          0x13,
+
+    "CONVERT":        0x1C,
+    "NONCONVERT":     0x1D,
+    "ACCEPT":         0x1E,
+    "MODECHANGE":     0x1F,
+
+    "SPACE":          0x20,
+    "PRIOR":          0x21,
+    "NEXT":           0x22,
+    "END":            0x23,
+    "HOME":           0x24,
+    "SELECT":         0x29,
+    "PRINT":          0x2A,
+    "EXECUTE":        0x2B,
+    "SNAPSHOT":       0x2C,
+    "INSERT":         0x2D,
+    "DELETE":         0x2E,
+    "HELP":           0x2F,
+    "SLEEP":          0x5F,
+};
+
+Object.entries(Keys).forEach(([k, v]) => Keys[v] = k);
+
+class KeyHook {
+    constructor() {
+        // start the keyboard hook
+        this.process = require("child_process").spawn("keyhook.exe");
+
+        // general logging
+        // this.process.on("spawn", () => console.log("Keyboard hook started!"));
+        this.process.stderr.on("data", (data) => console.error(`${data}`));
+        
+        // handle output of the keyboard hook
+        this.process.stdout.on("data", (data) => {
+            for (const line of data.toString().split(/[\n\r]+/)) {
+                const parts = line.split(",").map(x => parseInt(x, 10));
+                const [down, key, time] = parts;
+                
+                if (parts.length !== 3 || parts.some(isNaN)) continue;
+                this._processKey(down === 1, key, time);
+            }
+        });  
+    
+        // general initialization
+        for (let i=0; i<256; i++)
+            this._keyState[i] = false;
+    }
+        
+    _listeners = {};
+    _keyState = [];
+    _processKey(down, keyCode, time) {
+        const oldState = this._keyState[keyCode] == true;
+        this._keyState[keyCode] = down;
+        const key = Keys[keyCode];
+        let type = "unknown";
+
+        if (oldState && down)
+            type = "repeat";
+        else if (down)
+            type = "down";
+        else
+            type = "up";
+
+        if (!this._listeners[type]) return;
+        for (const listener of this._listeners[type]) {
+            listener({
+                type, 
+                keyCode, 
+                key, 
+                alt: this.isAltDown,
+                ctrl: this.isCtrlDown,
+                shift: this.isShiftDown,
+                time,
+            });
+        }
+    }
+  
+    get isShiftDown() { return this._keyState[Keys.L_SHIFT] || this._keyState[Keys.R_SHIFT] || this._keyState[Keys.SHIFT]; }
+    get isCtrlDown()  { return this._keyState[Keys.L_CTRL]  || this._keyState[Keys.R_CTRL]  || this._keyState[Keys.CTRL]; }
+    get isAltDown()   { return this._keyState[Keys.L_ALT]   || this._keyState[Keys.R_ALT]   || this._keyState[Keys.ALT]; }
+
+    isKeyDown(key) { 
+        if (typeof key === 'string') key = Keys[key.toUpperCase()];
+        return this._keyState[key];
+    }
+    
+    on(event, callback) {
+        if (this._listeners[event] === undefined)
+            this._listeners[event] = [];
+        this._listeners[event].push(callback);
+    }
+
+    registerShortCut(shortCut, callback) {
+        // transform the shortcut into an array of key codes
+        if (typeof shortCut === 'number')
+            shortCut = [shortCut];
+        if (typeof shortCut === 'string')
+            shortCut = shortCut.toUpperCase().split("+").map(x => Keys[x.trim()]);
+            shortCut = shortCut.map(x => {
+            if (typeof x === 'string') x = Keys[x.toUpperCase()];
+            if (x === Keys.L_SHIFT || x === Keys.R_SHIFT) x = Keys.SHIFT;
+            if (x === Keys.L_CTRL || x === Keys.R_CTRL) x = Keys.CTRL;
+            if (x === Keys.L_ALT || x === Keys.R_ALT) x = Keys.ALT;
+            return x;
+        });
+
+        const checkAlt = shortCut.includes(Keys.ALT);
+        const checkCtrl = shortCut.includes(Keys.CTRL);
+        const checkShift = shortCut.includes(Keys.SHIFT);
+        const checkKeys = shortCut.filter(x => x !== Keys.ALT && x !== Keys.CTRL && x !== Keys.SHIFT);
+
+        if (checkKeys.some(x => typeof x !== 'number')) 
+            throw new Error("Invalid key code");
+
+        this.on("down", (data) => {
+            if (
+                checkAlt === data.alt &&
+                checkCtrl === data.ctrl &&
+                checkShift === data.shift &&
+                checkKeys.every(x => this.isKeyDown(x))
+            ) {
+                callback();
+            }
+        });
+    }
+
+    stop() {
+        return this.process.kill();
+    }
+}
+
+const keyhook = new KeyHook();
+
+const template = /*html*/`<div></div>`
+
+module.exports = {
+    template,
+    mounted() {
+        //F13
+        keyhook.registerShortCut('F13', async () => { // Show IVs
+            console.log("Key: F13 pressed: EVs displayed.")
+            this.stats_display = "DVs";
+        });
+        //F14
+        keyhook.registerShortCut('F14', async () => { // Show EVs
+            console.log("Key: F14 pressed: IVs displayed.")
+            if (this.stats_display == "EVs") {
+                this.stats_display = "Detailed EVs";
+                return
+            }
+            this.stats_display = "EVs";
+        });
+        //F15
+        keyhook.registerShortCut('F15', async () => { // Show Base Stats
+            console.log("Key: F15 pressed. Base Stats displayed.")
+            this.stats_display = "Base Stats";
+        });
+        //F16
+        keyhook.registerShortCut('F16', async () => { // Automatic
+            console.log("Key: F16 pressed. Stats are displaying automatically now.")
+            if (this.stats_display == "Automatic") {
+                this.stats_display = "Badge Boosts";
+                return
+            }
+            this.stats_display = "Automatic";
+        });
+        //F17
+        keyhook.registerShortCut('F17', async () => { // Inventory
+            console.log("Key: F17 pressed. Automatic battle graphics will be displayed.")
+            if (this.disallow_right_panel_switching == true) { 
+                this.right_panel = "Automatic"
+                return 
+            }
+            this.right_panel = "Automatic";
+        });
+        //F18
+        keyhook.registerShortCut('F18', async () => { // Trainer Graphic during Battle
+            console.log("Key: F18 pressed. Splits displayed.")
+            if (this.disallow_right_panel_switching == true) { 
+                this.right_panel = "Automatic"
+                return 
+            }
+            this.right_panel = "Movepool";
+        });
+        //F19
+        keyhook.registerShortCut('F19', async () => { // Trainer Graphic during Battle
+            console.log("Key: F19 pressed. Movepool displayed.")
+            if (this.disallow_right_panel_switching == true) { 
+                this.right_panel = "Automatic"
+                return 
+            }
+            this.right_panel = "Splits";
+        });
+        //F20 used 
+        //F21 used
+        //F22 unused
+        //F23
+        keyhook.registerShortCut('F23', async () => {
+            this.newRun()
+        });
+        //F24
+        keyhook.registerShortCut('F24', async () => {
+            this.timer.pauseUnpauseTime();
+        });
+    }
+}
