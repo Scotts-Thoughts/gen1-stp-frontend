359a4efc138d5e464f2e7dbe0f3e4f953b1feefe
StringEpsilon <StringEpsilon@gmail.com>
2025-08-07 21:12:12 +0200

    Some cleanup in frontend.vue

--- src/renderer/components/frontend.vue
+++ src/renderer/components/frontend.vue
@@ -40,7 +40,7 @@ export default defineComponent({
 
         left_panel, // Left Panel
         game_area, // Middle Panel
-        right_panel //Right Panel
+        right_panel //LiRight Panel
     },
     props: ["mapper"],
     data() {
@@ -70,75 +70,82 @@ export default defineComponent({
         load_split_settings() {
             this.previous_splits = LocalStorageProxy['previous_splits'] as unknown as [] ?? []
         },
-    },
-    mounted: async function () {
-        this.load_split_settings();
-        this.autosplitterStore.configure(
-            this.mapper, 
-            createFullSplitGen1, 
-            getUniqueTrainerIdGen1, 
-            this.meta.game === "Yellow" ? yellow_splits : red_blue_splits
-        );
-        onStoreAction(this.metrics, "clear_run", this.clear_splits_header_timer);
+        setup_splitter() {
+            this.load_split_settings();
+            this.autosplitterStore.configure(
+                this.mapper, 
+                createFullSplitGen1, 
+                getUniqueTrainerIdGen1, 
+                this.meta.game === "Yellow" ? yellow_splits : red_blue_splits
+            );
+            onStoreAction(this.metrics, "clear_run", this.clear_splits_header_timer);
 
-        // Gen1 specific mapper watching. Everything else handled by the invoked stores (or the published events):
+            // Gen1 specific mapper watching. Everything else handled by the invoked stores (or the published events):
 
-        // RESET - Identifies when a reset occurs and publishes an event
-        this.mapper.properties.player.playerId.change((newProp, oldProp) => {
-            if (newProp.value == 0 && oldProp.value > 0 && this.metrics.state <= RunState.started) {
-                PubSub.publish("@run/reset_occurred");
-            }
-        });
-        // BLACKOUT - Identifies when a blackout might have occurred
-        this.mapper.properties.meta.state.change((newProp, oldProp) => {
-            PubSub.publish("@run/check_blackout", [newProp, oldProp]);
-            if (newProp.value == "To Battle" && this.mapper.properties.battle.type.value == "Trainer") {
-                this.settings.clearRightPanelOverride();
-            }
-        });
-        // NEW_RUN_STARTED - Determines if the player has pressed 'New Game' and started a new playthrough
-        this.mapper.properties.player.playerId.change((newProp) => {
-            if (newProp.value > 0 && this.metrics.state < RunState.saved && newProp.value != this.metrics.player_id) {
-                this.metrics.start_new_run(newProp.value);
-            }
-        });
-        // Only allow the player to reset once after the champion without resets incrementing
-        this.mapper.properties.player.name.change((newProp) => {
-            if (this.metrics.state == RunState.finished && newProp.value == "NINTEN") {
-                this.metrics.state = RunState.started;
-            }
-        });
-
-        // Check for the start of the battle:
-        this.mapper.properties.battle.type.change((newProp) => {
-            this.autosplitterStore.onBattleStart(newProp.value);
-        });
-
-        // the `lowHealthAlarm` property is used to play the Red-bar sound effect
-        // it is turned off as soon as "Player defeated Trainer" starts to render in the textbox
-		this.mapper.properties.battle.lowHealthAlarm.change((prop) => {
-			if (prop.value === "Disabled") {
-                this.autosplitterStore.onBattleEnd(this.mapper.properties.battle.type.value)
-            }
-		});
-
-        // when the triangular cursor appears on the screen, check if the game has ended:
-        this.mapper.properties.screen.text.prompt.change((newProp, oldProp) => {
-            if (newProp.bytes[0] == 0xEE && oldProp.bytes[0] == 0x7F) {
-                this.autosplitterStore.checkGameEnd();
-            }
-        });
-
-        // log the final times with the final gametime
-        // I am watching tile1 for a specifc tile that appears when the gametime displays on screen
-        this.mapper.properties.screen.tiles.column1.tile1.change((newProp) => {
-            if (newProp.value == 122) {
-                if (this.mapper.properties.events.beatChampion.value == true && this.mapper.properties.overworld.map.value == "Hall of Fame") {
-                    this.autosplitterStore.onFinalSplit();
+            // RESET - Identifies when a reset occurs and publishes an event
+            this.mapper.properties.player.playerId.change((newProp, oldProp) => {
+                if (newProp.value == 0 && oldProp.value > 0 && this.metrics.state <= RunState.started) {
+                    PubSub.publish("@run/reset_occurred");
                 }
-            }
-        });
+            });
+            // BLACKOUT - Identifies when a blackout might have occurred
+            this.mapper.properties.meta.state.change((newProp, oldProp) => {
+                PubSub.publish("@run/check_blackout", [newProp, oldProp]);
+                if (newProp.value == "To Battle" && this.mapper.properties.battle.type.value == "Trainer") {
+                    this.settings.clearRightPanelOverride();
+                }
+            });
+            // NEW_RUN_STARTED - Determines if the player has pressed 'New Game' and started a new playthrough
+            this.mapper.properties.player.playerId.change((newProp) => {
+                // TODO: this may not be correct. See comment on RunState.saved. Also compare below.
+                if (newProp.value > 0 && this.metrics.state < RunState.saved && newProp.value != this.metrics.player_id) {
+                    this.metrics.start_new_run(newProp.value);
+                }
+            });
 
+            // Only allow the player to reset once after the champion without resets incrementing
+            this.mapper.properties.player.name.change((newProp) => {
+                // TODO: this may not be correct. See comment on RunState.finished.
+                if (this.metrics.state == RunState.finished && newProp.value == "NINTEN") {
+                    this.metrics.state = RunState.started;
+                }
+            });
+
+            // Check for the start of the battle:
+            this.mapper.properties.battle.type.change((newProp) => {
+                this.autosplitterStore.onBattleStart(newProp.value);
+            });
+
+            // the `lowHealthAlarm` property is used to play the Red-bar sound effect
+            // it is turned off as soon as "Player defeated Trainer" starts to render in the textbox
+            this.mapper.properties.battle.lowHealthAlarm.change((prop) => {
+                if (prop.value === "Disabled") {
+                    this.autosplitterStore.onBattleEnd(this.mapper.properties.battle.type.value)
+                }
+            });
+
+            // when the triangular cursor appears on the screen, check if the game has ended:
+            this.mapper.properties.screen.text.prompt.change((newProp, oldProp) => {
+                if (newProp.bytes[0] == 0xEE && oldProp.bytes[0] == 0x7F) {
+                    this.autosplitterStore.checkGameEnd();
+                }
+            });
+
+            // log the final times with the final gametime
+            // I am watching tile1 for a specifc tile that appears when the gametime displays on screen
+            this.mapper.properties.screen.tiles.column1.tile1.change((newProp) => {
+                if (newProp.value == 122) {
+                    if (this.mapper.properties.events.beatChampion.value == true && this.mapper.properties.overworld.map.value == "Hall of Fame") {
+                        this.autosplitterStore.onFinalSplit();
+                    }
+                }
+            });
+        }
+    },
+    mounted: function () {
+        this.setup_splitter();
+
+        // TODO: move to encounter.vue component.
         // Alakazam Yellow exception
         this.mapper.properties?.trainers?.viridianForest?.bugcatcher2.change((newValue) => {
             if (this.meta.game == "Yellow" && this.meta.starter == "Alakazam" && newValue.value == true) {
